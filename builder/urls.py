3cac9f6e1c5ebbcce537d3cf9f022b52e79e5276
Unknown <zurkova.leona@email.cz>
2018-12-14 19:14:44 +0100

    Right comments for right decks

--- builder/urls.py
+++ builder/urls.py
@@ -9,27 +9,27 @@ urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),   # TODO signup
 
     path('search/', views.search, name='search'),  # search field
-    path('search/result', views.result, name='result'),  # results of searching
-    path('search/result/<str:card_id>', views.detail, name='detail'),  # detail of card
+    path('search/result/', views.result, name='result'),  # results of searching
+    path('search/result/<str:card_id>/', views.detail, name='detail'),  # detail of card
 
     path('players/', views.players, name='players'),  # all players
-    path('player/<int:player_id>', views.player, name='player'),  # detail of player
+    path('player/<int:player_id>/', views.player, name='player'),  # detail of player
 
     #path('new_player/', views.new_player, name='new_player'),
     #path('new_player_submit/', views.new_player_submit, name='new_player_submit'),
 
     path('decks/', views.decks, name='decks'),  # all decks
-    path('deck/<int:deck_id>', views.deck, name='deck'),  # detail of deck
+    path('deck/<int:deck_id>/', views.deck, name='deck'),  # detail of deck
 
-    path('create_deck/<str:card_id>', views.create_deck, name='create_deck'),
-    path('create_deck_submit/<str:card_id>', views.create_deck_submit, name='create_deck_submit'),
-    path('add_to_deck/<str:card_id>', views.add_to_deck, name='add_to_deck'),
-    path('add_to_deck_submit/<str:card_id>', views.add_to_deck_submit, name='add_to_deck_submit'),
+    path('create_deck/<str:card_id>/', views.create_deck, name='create_deck'),
+    path('create_deck_submit/<str:card_id>/', views.create_deck_submit, name='create_deck_submit'),
+    path('add_to_deck/<str:card_id>/', views.add_to_deck, name='add_to_deck'),
+    path('add_to_deck_submit/<str:card_id>/', views.add_to_deck_submit, name='add_to_deck_submit'),
 
-    path('delete_card/<int:id>', views.delete_card, name='delete_card'),
-    path('delete_deck/<int:id>', views.delete_deck, name='delete_deck'),
+    path('delete_card/<int:id>/', views.delete_card, name='delete_card'),
+    path('delete_deck/<int:id>/', views.delete_deck, name='delete_deck'),
 
-    path('new_comment/<int:deck_id>', views.new_comment, name='new_comment'),
-    path('new_comment_submit/<int:deck_id>', views.new_comment_submit, name='new_comment_submit'),
+    path('new_comment/<int:deck_id>/', views.new_comment, name='new_comment'),
+    path('new_comment_submit/<int:deck_id>/', views.new_comment_submit, name='new_comment_submit'),
 
 ]
